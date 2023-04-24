e8a07293ed5b61e213dda5ee41f5f674fbde07bb
Anhisa <anhisa@hotmail.com>
2023-04-24 13:37:33 -0500

    conflicts fixed

--- src/components/CompanyPage/CompanyContextSection.jsx
+++ src/components/CompanyPage/CompanyContextSection.jsx
@@ -9,24 +9,21 @@ export function CompanyContextSection() {
   let companySect = CompanyInfo().CompaniesInfo.companies_Sectors;
   let { C_ID } = useParams();
 
-  const infoCompany = [];
-  infoCompany.push(companyData.find((info) => info.COMPANY_NAME_SPA === C_ID));
-  const CompanySectors = [];
-  CompanySectors.push(companySect.filter((info) => info.COMPANY_NAME_SPA === C_ID));
-
+  const infoCompany = companyData.find((info) => info.COMPANY_NAME_SPA === C_ID);
+  const CompanySectors = companySect.filter((info) => info.COMPANY_NAME_SPA === C_ID);
   return (
     <>
       <section className="container-company mt-64 mb-140 p-1">
         <div className="box-header-company">
           <h4 className="font-m-p">CONTEXTO EMPRESA</h4>
-          <p>{infoCompany[0]?.DESCRIPTION_OF_OPERATIONS_IN_VENEZUELA_SPA}</p>
+          <p>{infoCompany?.DESCRIPTION_OF_OPERATIONS_IN_VENEZUELA_SPA}</p>
           <div className="box-header-dates">
             <div className="box-dates">
               <span>Sectores</span>
             </div>
             <div className="box-dates">
               <ul className="icons-projects-sectores">
-                {CompanySectors[0].map((sector) => (
+                {CompanySectors.map((sector) => (
                   <Link key={`link-${sector?.SECTOR_NAME_SPA}`} style={{textDecoration: "none"}} to={`/sector/${sector?.SECTOR_NAME_SPA}`}>
                     <div className="text-align">
                       <div className="box-icons-project text-icons-project">
